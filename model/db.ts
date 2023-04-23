9ce448b6324f63ba7e650056ba23846d32367c50
gh0st3e <bigdenbstu@yandex.by>
2023-04-23 06:02:02 +0300

    api v1.1.6

--- model/db.ts
+++ model/db.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize'
 import {Dialect} from 'sequelize/types';
 import {DB_CONFIG} from '../config/db.config'
-import {diskStorage} from "multer";
+
 
 
 const sequelize = new Sequelize(
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     DB_CONFIG.PASSWORD!,
     {
         host: DB_CONFIG.HOST,
-        dialect: 'postgres'
+        dialect: DB_CONFIG.DIALECT as Dialect
     }
 );
 
